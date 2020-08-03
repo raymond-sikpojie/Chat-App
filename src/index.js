88b0c7dbd2606840b67493c75613f5308c6c17f7
raymond <raymondomon@yahoo.com>
2020-08-03 10:02:09 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const socketio = require("socket.io");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Create server for socket io
+const server = http.createServer(app);
+
+// Initialize socket
+const io = socketio(server);
+
+// Add eventListener to io
+io.on("connection", () => {
+  console.log("New web socket connection");
+});
+
+// Serve static assets
+const publicDirectoryPath = path.join(__dirname, "../public");
+app.use(express.static(publicDirectoryPath));
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
